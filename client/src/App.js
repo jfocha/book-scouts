56b34636b41206595266652d6d5e7a31c708086b
jfocha <jfocha@gmail.com>
2021-09-29 22:04:16 -0700

    app setup

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+// import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+
+// const httpLink = createHttpLink({
+//   uri: 'http://localhost:3001/graphql',
+// });
+
+// const client = new ApolloClient({
+//   link: httpLink,
+//   cache: new InMemoryCache(),
+// });
+
+function App() {
+  return (
+    <div></div>
+    // <ApolloProvider client={client}>
+    //   <div className="flex-column justify-flex-start min-100-vh">
+    //     <Header />
+    //     <div className="container">
+    //       <Home />
+    //     </div>
+    //     <Footer />
+    //   </div>
+    // </ApolloProvider>
+  );
+}
+
+export default App;
