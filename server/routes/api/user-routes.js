c2ab2e12c1c0e65def0a83ba06a39ecbc45c71a4
Deeparkrish <kosalai17@gmail.com>
2021-09-29 23:09:38 -0700

    api routes for mongo

--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -12,11 +12,11 @@ const { authMiddleware } = require('../../utils/auth');
 
 // put authMiddleware anywhere we need to send a token for verification of user
 router.route('/').post(createUser).put(authMiddleware, saveBook);
-
+// login route 
 router.route('/login').post(login);
-
+// get a user 
 router.route('/me').get(authMiddleware, getSingleUser);
-
+// remove a book from wish list 
 router.route('/books/:bookId').delete(authMiddleware, deleteBook);
 
 module.exports = router;
