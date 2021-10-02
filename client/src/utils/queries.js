acce4dd4322f3b5c6716a5e36a003c76dd83eee6
Deeparkrish <kosalai17@gmail.com>
2021-10-01 17:15:53 -0700

    queries and mutuations basic

---
+++ client/src/utils/queries.js
@@ -0,0 +1,61 @@
+import { gql } from '@apollo/client';
+
+// Query a User 
+export const QUERY_ME = gql`
+{
+  me {
+    _id
+    username
+    email
+    bookCount
+    booksCheckedOut {
+        bookId
+        author
+        title
+        description
+        image
+        link
+    }
+  }
+}`;
+
+// Query users and books they checked out 
+export const QUERY_USERS =gql`
+{
+    users{
+      _id
+      username 
+      booksCheckedOut{
+        title
+        _id
+        ISBN
+        author
+        borrowers{
+          username
+          _id
+        }
+      }
+    }
+  }
+`;
+// Query all Books 
+export const QUERY_BOOK = gql`
+{
+    books {
+      ISBN
+      _id
+      title
+      borrowers{
+        _id
+        username
+      }
+    }
+  }
+`;
+
+  // Query by userName 
+export const QUERY_BYUSERNAME =gql`
+    
+     
+
+`;
