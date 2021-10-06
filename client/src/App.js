84bd489f99d4bb8262e34a3b3ac973fa2771c3c8
Richard Steavens <rasteavens@yahoo.com>
2021-10-06 16:15:42 -0700

    merge from main

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,22 @@
-import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { ApolloProvider } from '@apollo/react-hooks';
-// import ApolloClient from 'apollo-boost';
-import { useState } from 'react';
-import ModalDialog from './components/ModalDialog';
-import PrimarySearchAppBar from '././components/Navbar'
-import EnhancedTable from './components/BookTable';
-import SimplePaper from './components/SearchedBooks';
-import Cart from './components/Checkout';
-import { setContext } from '@apollo/client/link/context';
+// import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
 
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('token');
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    }
-  }
-});
+// const httpLink = createHttpLink({
+//   uri: 'http://localhost:3001/graphql',
+// });
 
-const client = new ApolloClient({
-  // request: operation => {
-  //   const token = localStorage.getItem('id_token');
+// const client = new ApolloClient({
+//   link: httpLink,
+//   cache: new InMemoryCache(),
+// });
 
-<<<<<<< HEAD
+
 import { useState } from 'react';
 import ModalDialog from './ModalDialog';
 import PrimarySearchAppBar from './Navbar';
-import Navbar from './Signup';
-=======
-  //   operation.setContext({
-  //     headers: {
-  //       authorization: token ? `Bearer ${token}` : ''
-  //     }
-  //   });
-  // },
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
->>>>>>> develop
-
 const App = () => {
   // declare a new state variable for modal open
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
 
   // function to handle modal open
   const handleOpen = () => {
@@ -62,17 +29,10 @@ const App = () => {
   };
 
   return (
-    <ApolloProvider client={client}>
     <div className="App">
-      <div>
       <ModalDialog open={open} handleClose={handleClose} />
       <PrimarySearchAppBar />
-      </div>
-      <div><SimplePaper /></div>
-      <div><EnhancedTable /></div>
-      {/* <div><Cart /></div> */}
     </div>
-    </ApolloProvider>
   );
 };
 
