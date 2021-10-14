512429953f8b59e6b9c3c0b8365db893e3f31af6
jfocha <jfocha@gmail.com>
2021-10-13 22:48:59 -0700

    added pagination

---
+++ client/src/components/DisplayBooks/index.js
@@ -0,0 +1,106 @@
+import * as React from 'react';
+import { useState } from "react";
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import { Typography } from '@mui/material';
+import usePagination from '../AppPagination';
+import Pagination from '@mui/material/Pagination';
+import Auth from '../../utils/auth';
+import { useMutation } from '@apollo/react-hooks';
+import { CHECKOUT_BOOK } from '../../utils/mutations';
+
+export default function DisplayBooks(props) {
+    const [checkoutBookData, { error }] = useMutation(CHECKOUT_BOOK);
+    console.log(error);
+
+    let [page, setPage] = useState(1);
+    const PER_PAGE = 10;
+
+    const count = Math.ceil(props.data.length / PER_PAGE);
+    const _DATA = usePagination(props.data, PER_PAGE);
+
+    const handleChange = (e, p) => {
+        setPage(p);
+        _DATA.jump(p);
+        console.log("Data " + props.data.books)
+    };
+
+    const checkoutBookHandler = (bookNumber) => {
+        console.log("checkoutBookHandler " + loggedIn + " " + typeof bookNumber + " " + bookNumber);
+        checkoutBookData({
+            variables: { checkoutBookBookId: bookNumber }
+        });
+    };
+
+    const loggedIn = Auth.loggedIn();
+
+    const centerFormat = {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: "15px",
+    }
+
+    return (
+        <>
+            <Box
+                py={3}
+                style={{
+                    stickToTop: {
+                        width: '100%',
+                        position: 'fixed',
+                        top: 0,
+                    }
+                }}>
+                <Pagination
+                    count={count}
+                    page={page}
+                    onChange={handleChange}
+                    variant="outlined"
+                    shape="rounded" />
+            </Box>
+            <div
+                style={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    flexDirection: 'row'
+                }}
+            >
+                {_DATA.currentData().map((book, i) => (
+                    <Box
+                        key={book._id}
+                        sx={{
+                            display: 'flex',
+                            flexWrap: 'wrap',
+                            '& > :not(style)': {
+                                m: 1,
+                                width: 300,
+                                height: 320,
+                                transform: 'translateZ(0px)',
+                            },
+                        }}
+                    >
+                        <Paper elevation={5}>
+                            <div style={centerFormat}>
+                                <img src={book.ISBN} alt="" style={{
+                                    height: 180,
+                                    width: 135,
+                                }} />
+                            </div>
+                            <Typography noWrap align={"center"} variant="body1"
+                            >{book.title}</Typography>
+                            <Typography noWrap align={"center"} variant="body2">by {book.author}</Typography>
+                            <div style={{ padding: "15px" }}>
+                                <Stack spacing={2} direction="row">
+                                    <Button variant="contained" fullWidth onClick={() => checkoutBookHandler(book._id)}>Checkout</Button>
+                                </Stack>
+                            </div>
+                        </Paper>
+                    </Box>
+                ))}
+            </div>
+        </>
+    );
+}
--- client/src/components/SearchedBooks/index.js
+++ client/src/components/SearchedBooks/index.js
@@ -1,92 +1,22 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import { Typography } from '@mui/material';
-import Pagination from '@mui/material/Pagination';
-import Auth from '../../utils/auth';
+import DisplayBooks from '../DisplayBooks';
 import { QUERY_BOOK } from '../../utils/queries';
 import { useQuery } from '@apollo/client';
-import { useMutation } from '@apollo/react-hooks';
-import { CHECKOUT_BOOK } from '../../utils/mutations';
 
 export default function SimplePaper() {
-    const [checkoutBookData, { error }] = useMutation(CHECKOUT_BOOK);
-    console.log(error);
-
     const { loading, data } = useQuery(QUERY_BOOK);
     if (loading) {
         return <h1>Loading...</h1>
     }
     const getAllBooks = data?.books || [];
 
-    const books = getAllBooks.map(book => {
+    const bookDisplay = getAllBooks.map(book => {
         return book;
     })
 
-    const loggedIn = Auth.loggedIn();
-
-    const checkoutBookHandler = (bookNumber) => {
-        console.log("checkoutBookHandler " + loggedIn + " " + typeof bookNumber + " " + bookNumber);
-        checkoutBookData({
-            variables: { checkoutBookBookId: bookNumber }
-        });
-    };
-
-    const centerFormat = {
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        padding: "15px",
-    }
-
     return (
-        <div
-            style={{
-                display: 'flex',
-                flexWrap: 'wrap',
-                flexDirection: 'row'
-            }}>
-                
-            {books.map((book, i) => (
-                <Stack spacing={2} direction="row">
-                <Box
-                    key={i}
-                    // noWrap
-                    sx={{
-                        display: 'flex',
-                        flexWrap: 'wrap',
-                        '& > :not(style)': {
-                            m: 1,
-                            width: 300,
-                            height: 320,
-                            transform: 'translateZ(0px)',
-                        },
-
-                    }}
-                >
-                    <Paper elevation={5}>
-                        <div style={centerFormat}>
-                            <img src={books[i].ISBN} alt="" style={{
-                                height: 180,
-                                width: 135,
-                            }} />
-                        </div>
-                        <Typography noWrap align={"center"} variant="body1"
-                        >{books[i].title}</Typography>
-                        <Typography noWrap align={"center"} variant="body2">by {books[i].author}</Typography>
-                        <div style={{ padding: "15px" }}>
-                            {/* <Stack spacing={2} direction="row"> */}
-                                <Button variant="contained" fullWidth onClick={() => checkoutBookHandler(books[i]._id)}>Checkout</Button>
-                            {/* </Stack> */}
-                        </div>
-                    </Paper>
-                </Box>
-                {/* <Pagination count={10} variant="outlined" shape="rounded" /> */}
-                </Stack>
-            ))}
-            
+        <div>
+<DisplayBooks data={bookDisplay} />
         </div>
     );
 }
